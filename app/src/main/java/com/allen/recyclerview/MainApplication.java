ed9197f57d58e2709bf37b54940018d3c8cff52b
Allen <AllenCoder@126.com>
2016-11-16 10:41:03 +0800

    add tinker sample

---
+++ app/src/main/java/com/allen/recyclerview/MainApplication.java
@@ -0,0 +1,28 @@
+/*
+******************************* Copyright (c)*********************************\
+**
+**                 (c) Copyright 2015, Allen, china, shanghai
+**                          All Rights Reserved
+**
+**                          
+**                         
+**-----------------------------------版本信息------------------------------------
+** 版    本: V0.1
+**
+**------------------------------------------------------------------------------
+********************************End of Head************************************\
+*/
+package com.allen.recyclerview;
+
+import android.app.Application;
+
+/**
+ * 文 件 名: MainApplication
+ * 创 建 人: Allen
+ * 创建日期: 16/11/16 10:34
+ * 邮   箱: AllenCoder@126.com
+ * 修改时间：
+ * 修改备注：
+ */
+public class MainApplication extends Application {
+}
